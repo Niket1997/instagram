338ec7a041b62bfe5dc973901e118b956f02e216
aniketmahangare <aniket.mahangare@jupiter.money>
2023-12-19 18:06:44 +0530

    rename upload service to resource service

--- resourceservice/src/main/java/org/instagram/ResourceServiceApplication.java
+++ resourceservice/src/main/java/org/instagram/ResourceServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UploadServiceApplication {
+public class ResourceServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UploadServiceApplication.class, args);
+        SpringApplication.run(ResourceServiceApplication.class, args);
     }
 }
