b66408e067bfee668972a93389a0386248a84e23
aniketmahangare <aniket.mahangare@jupiter.money>
2023-12-24 16:55:06 +0530

    working post with kafka producer

--- postservice/src/main/java/org/instagram/configs/KafkaProducerConfig.java
+++ postservice/src/main/java/org/instagram/configs/KafkaProducerConfig.java
@@ -1,6 +1,5 @@
 package org.instagram.configs;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.instagram.records.post.PostResponse;
@@ -10,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
